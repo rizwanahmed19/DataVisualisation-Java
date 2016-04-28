b9731597367782b8d808f9d3c451689f930fb3eb
Rizwan Ahmed <rizwan.ahmed19@outlook.com>
2016-04-28 18:17:04 +0500

    Add a helper method to extract data from csv files

--- src/LifeExpectancy/LifeExpectancy.java
+++ src/LifeExpectancy/LifeExpectancy.java
@@ -1,10 +1,11 @@
 package LifeExpectancy;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
-import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.*;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,15 +14,35 @@ import java.util.List;
 public class LifeExpectancy extends PApplet {
 
     private UnfoldingMap map; // To display the map
+    private HashMap<String, Float> lifeExpData;
 
     public void setup(){
-        size(800, 600);
+        size(700, 500);
 
         // Creates a map on this PApplet of width 700 and height 500
-        map = new UnfoldingMap(this, 50, 50, 700, 500, new Google.GoogleMapProvider());
+        map = new UnfoldingMap(this, 50, 50, 600, 400, new Google.GoogleMapProvider());
         MapUtils.createDefaultEventDispatcher(this, map); // make the map interactive
         map.setTweening(true); // make zooming in and out smooth
+        map.zoomToLevel(1);
+        lifeExpData = loadLifeExpData("G:\\IdeaProjects\\CourseraJava\\data\\LifeExpectancyWorldBank.csv");
 
+    }
+
+    /*
+    *  This helper method takes a .csv file name and extracts country id and
+    *  average life expectancy value of each country and returns a HashMap that contains those values.
+    */
+    private HashMap<String, Float> loadLifeExpData(String fileName) {
+        HashMap<String, Float> lifeExpData = new HashMap<>();
+        String rows[] = loadStrings(fileName); // Splits the file into rows
+
+        for(String row : rows){
+            String[] columns = row.split(","); // divide each row into columns wherever a coma occurs
+            if (columns.length == 6 && !columns[5].equals("..")){
+                lifeExpData.put(columns[4], Float.parseFloat(columns[5]));
+            }
+        }
+        return lifeExpData;
     }
 
     public void draw(){
